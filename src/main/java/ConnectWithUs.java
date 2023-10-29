0490da6b566b30a18b8e2c6cb8d7dc53cc95fe9f
PershikovaEP <zem524@mail.ru>
2023-10-29 16:22:47 +0300

    update

--- src/main/java/ConnectWithUs.java
+++ src/main/java/ConnectWithUs.java
@@ -7,14 +7,30 @@ import static com.codeborne.selenide.Condition.visible;
 
 
 public class ConnectWithUs {
-    private SelenideElement name = Selenide.$x("//*[@id='input_1495810359387']");
-    private SelenideElement lastName = Selenide.$x("//*[@id='input_1653564351806']");
-    private SelenideElement email = Selenide.$x("//*[@id='input_1495810354468']");
-    private SelenideElement phone = Selenide.$x("//*[@id='input_1669804191159']");
-    private SelenideElement company = Selenide.$x("//*[@id='input_1495810410810']");
-    private SelenideElement job = Selenide.$x("//*[@id='input_1653564461294']");
-    private SelenideElement question = Selenide.$x("//*[@id='input_1653564485974']");
-    private SelenideElement button = Selenide.$(".t-form__submit button");
+
+    //Использую абсолютный путь, так как в DOM-дереве форма Связаться с нами представлена 3 раза
+      private SelenideElement name = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[1]/div/input");
+    //private SelenideElement name = Selenide.$x("//*[@id='input_1495810359387']");
+
+    private SelenideElement lastName = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[2]/div/input");
+    //private SelenideElement lastName = Selenide.$x("//*[@id='input_1653564351806']");
+    private SelenideElement email = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[3]/div/input");
+    //private SelenideElement email = Selenide.$x("//*[@id='input_1495810354468']");
+
+    private SelenideElement phone = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[4]/div/input");
+   // private SelenideElement phone = Selenide.$x("//*[@id='input_1669804191159']");
+
+    private SelenideElement company = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[5]/div/input");
+    //private SelenideElement company = Selenide.$x("//*[@id='input_1495810410810']");
+
+    private SelenideElement job = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[6]/div/input");
+    //private SelenideElement job = Selenide.$x("//*[@id='input_1653564461294']");
+
+    private SelenideElement question = Selenide.$x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[7]/div/textarea");
+   // private SelenideElement question = Selenide.$x("//*[@id='input_1653564485974']");
+
+    private SelenideElement button = Selenide.$("/html/body/div/div[8]/div/div/div[2]/div/form/div[2]/div[10]/button");
+    //private SelenideElement button = Selenide.$(".t-form__submit button");
 
     //В конструкторе проверяем, что страница открылась
     public ConnectWithUs() {
