7e8013097447e937f19eb5919e4e7049d0213151
PershikovaEP <zem524@mail.ru>
2023-10-29 15:01:31 +0300

    update

--- src/test/java/AuxoTest.java
+++ src/test/java/AuxoTest.java
@@ -20,44 +20,40 @@ public class AuxoTest {
     }
 
     @Test
+
     void shouldSendMessage() throws InterruptedException {
         $(".t228__right_buttons_but a").click();
 
-        //Элементы повторятся трижды
-        //Внесение в поле инпут значения после очистки ctrl + a и backspace с помощью sendKeys
-        $x("//*[@id='input_1495810359387']").sendKeys(Keys.CONTROL + "a");
-        $x("//*[@id='input_1495810359387']").sendKeys(Keys.BACK_SPACE);
-        $x("//*[@id='input_1495810359387']").sendKeys("Тест");
+        //В связи с тройным повтором элементов формы Обратной связи беру абсолютный путь
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[1]/div/input").setValue("Тест");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[2]/div/input").setValue("Тест");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[3]/div/input").setValue("test@mail.ru");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[4]/div/input").setValue("+79202020000");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[5]/div/input").setValue("Тест");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[6]/div/input").setValue("Менеджер");
+        $x("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[7]/div/textarea").setValue("Тест?");
 
-        //Внесение в поле инпут значения после очистки двойным кликом и backspace с помощью sendKeys
-        $x("//*[@id='input_1653564351806']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $x("//*[@id='input_1653564351806']").sendKeys("Тест");
-
-        //Внесение в поле инпут значения после двойного клики, очистки ctrl + a и backspace с помощью setValue
-        $x("//*[@id='input_1495810354468']").doubleClick().sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE );
-        $x("//*[@id='input_1495810354468']").setValue("test@mail.ru");
-
-        //Внесение в поле инпут значения после очистки двойным кликом и clear
-        $x("//*[@id='input_1495810354468']").doubleClick().clear();
-        $x("//*[@id='input_1495810354468']").setValue("test@mail.ru");
-
-        //Внесение в поле инпут значения после двойного клика, очистки ctrl + a и delete с помощью setValue
-        $x("//*[@id='input_1669804191159']").doubleClick().sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        $x("//*[@id='input_1669804191159']").setValue("+79202020000");
-
-        $x("//*[@id='input_1495810410810']").setValue("Тест");
-
-        $x("//*[@id='input_1653564461294']").setValue("Менеджер");
-
-        $x("//*[@id='input_1653564485974']").doubleClick().setValue("Тест?");
-
-        //задержка средствами java - не работает, элемента 3, они перекрывают друг друга
-        Thread.sleep(10000);
-        $(".t-form__submit button").click();
+        //селектор не доступен из-за перекрытия другими элементами
+        //Thread.sleep(10000); //задержка и скрол не помогли
+        //$("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[10]/button").scrollIntoView(true);
+        $("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[10]/button").click();
 
         $x("//*[@id='form521025490']/div[@class='js-successbox t-form__successbox t-text t-text_md']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(10))
                 .shouldBe(Condition.exactText("Спасибо! Данные успешно отправлены."));
+
+        //Код с более грамотными селекторами
+//        $x("//*[@id='input_1495810359387']").setValue("Тест");
+//        $x("//*[@id='input_1653564351806']").setValue("Тест");
+//        $x("//*[@id='input_1495810354468']").setValue("test@mail.ru");
+//        $x("//*[@id='input_1669804191159']").setValue("+79202020000");
+//        $x("//*[@id='input_1495810410810']").setValue("Тест");
+//        $x("//*[@id='input_1653564461294']").setValue("Менеджер");
+//        $x("//*[@id='input_1653564485974']").setValue("Тест?");
+//        $(".t-form__submit button").click();
+//        $x("//*[@id='form521025490']/div[@class='js-successbox t-form__successbox t-text t-text_md']")
+//                .shouldBe(Condition.visible, Duration.ofSeconds(10))
+//                .shouldBe(Condition.exactText("Спасибо! Данные успешно отправлены."));
 
     }
 }
