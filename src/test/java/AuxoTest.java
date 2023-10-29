636887d3ca5484466800cb078c753204a7af4b7c
PershikovaEP <zem524@mail.ru>
2023-10-29 12:40:00 +0300

    update test

--- src/test/java/AuxoTest.java
+++ src/test/java/AuxoTest.java
@@ -1,9 +1,11 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
 
@@ -18,9 +20,10 @@ public class AuxoTest {
     }
 
     @Test
-    void shouldSendMessage() {
+    void shouldSendMessage() throws InterruptedException {
         $(".t228__right_buttons_but a").click();
 
+        //Элементы повторятся трижды
         //Внесение в поле инпут значения после очистки ctrl + a и backspace с помощью sendKeys
         $x("//*[@id='input_1495810359387']").sendKeys(Keys.CONTROL + "a");
         $x("//*[@id='input_1495810359387']").sendKeys(Keys.BACK_SPACE);
@@ -48,7 +51,8 @@ public class AuxoTest {
 
         $x("//*[@id='input_1653564485974']").doubleClick().setValue("Тест?");
 
-        //scrollIntoView(false);
+        //задержка средствами java - не работает, элемента 3, они перекрывают друг друга
+        Thread.sleep(10000);
         $(".t-form__submit button").click();
 
         $x("//*[@id='form521025490']/div[@class='js-successbox t-form__successbox t-text t-text_md']")
