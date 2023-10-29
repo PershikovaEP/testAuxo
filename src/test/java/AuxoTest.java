4e9feadaaa6dd100a5f71f1bb30bbcf1c024ba1f
PershikovaEP <zem524@mail.ru>
2023-10-29 16:10:17 +0300

    update

--- src/test/java/AuxoTest.java
+++ src/test/java/AuxoTest.java
@@ -37,47 +37,37 @@ public class AuxoTest {
     }
 
     @Test
-    void shouldSendMessage() throws InterruptedException {
+    void shouldSendMessage() {
         driver.findElement(By.cssSelector(".t228__right_buttons_but a")).click();
 
-        //3 элемента этой формы! Ни одно поле не заполнилось и кнопка отправить не нажимается
-        //Через скрипт попробовала заполнить поле инпут
-        WebElement name = driver.findElement(By.xpath("//*[@id='input_1495810359387']"));
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-        js.executeScript("arguments[0].value = '';", name);
-        name.sendKeys("тест");
+        //В связи с 3 идентичными формами Связаться с нами использую абсолютный путь
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[1]/div/input")).sendKeys("Тест");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[2]/div/input")).sendKeys("Тест");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[3]/div/input")).sendKeys("test@mail.ru");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[4]/div/input")).sendKeys("+79202020000");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[5]/div/input")).sendKeys("Тест");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[6]/div/input")).sendKeys("Менеджер");
+        driver.findElement(By.xpath("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[7]/div/textarea")).sendKeys("Тест?");
 
-        //Внесение значения в поле инпут после очистки поля c помощью Backspace
-        driver.findElement(By.xpath("//*[@name = 'Name_2']")).sendKeys(Keys.BACK_SPACE);
-        driver.findElement(By.xpath("//*[@name = 'Name_2']")).sendKeys("Тест");
-
-        //Внесение значения в поле инпут после очистки  ctrl+a и backspace
-        driver.findElement(By.xpath("//*[@id='input_1495810354468']")).sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
-        driver.findElement(By.xpath("//*[@id='input_1495810354468']")).sendKeys("test@mail.ru");
-
-        //Внесение значения в поле инпут после очистки поля методом clear
-        driver.findElement(By.xpath("//*[@id='input_1495810354468']")).clear();
-        driver.findElement(By.xpath("//*[@id='input_1495810354468']")).sendKeys("test@mail.ru");
-
-        //Внесение значения в поле инпут после очистки  ctrl+a и delete
-        driver.findElement(By.xpath("//*[@id='input_1669804191159']")).sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        driver.findElement(By.xpath("//*[@id='input_1669804191159']")).sendKeys("+79202020000");
-
-        driver.findElement(By.xpath("//*[@id='input_1495810410810']")).sendKeys("Тест");
-
-        driver.findElement(By.xpath("//*[@id='input_1653564461294']")).sendKeys("Менеджер");
-
-        driver.findElement(By.xpath("//*[@id='input_1653564485974']")).sendKeys("Тест?");
-
-        //Не находит элемент, ставлю задержку через java или через средства selenium - не проходит, таких 3 элемента, они перекрывают друг друга
-        //Thread.sleep(10000);
-        //driver.findElement(By.cssSelector(".t-form__submit button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10000));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".t-form__submit button"))).click();
-        //wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".t-form__submit button"))).Click();
+        // Тут 3 кнопки и они перекрывают друг друга, поэтому нажатие на кнопку Отправить не происходит
+        driver.findElement(By.cssSelector("/html/body/div/div[17]/div/div/div[2]/div/form/div[2]/div[10]/button")).click();
 
         String actual = driver.findElement(By.xpath("//*[@id='form521025490']/div[@class='js-successbox t-form__successbox t-text t-text_md']")).getText();
         Assertions.assertEquals("Спасибо! Данные успешно отправлены.", actual);
+
+
+      //Код с более грамотными селекторами
+//        driver.findElement(By.xpath("//*[@id='input_1495810359387']")).sendKeys("Тест");
+//        driver.findElement(By.xpath("//*[@name = 'Name_2']")).sendKeys("Тест");
+//        driver.findElement(By.xpath("//*[@id='input_1495810354468']")).sendKeys("test@mail.ru");
+//        driver.findElement(By.xpath("//*[@id='input_1669804191159']")).sendKeys("+79202020000");
+//        driver.findElement(By.xpath("//*[@id='input_1495810410810']")).sendKeys("Тест");
+//        driver.findElement(By.xpath("//*[@id='input_1653564461294']")).sendKeys("Менеджер");
+//        driver.findElement(By.xpath("//*[@id='input_1653564485974']")).sendKeys("Тест?");
+//        driver.findElement(By.cssSelector(".t-form__submit button")).click();
+//
+//        String actual = driver.findElement(By.xpath("//*[@id='form521025490']/div[@class='js-successbox t-form__successbox t-text t-text_md']")).getText();
+//        Assertions.assertEquals("Спасибо! Данные успешно отправлены.", actual);
 
     }
 }
